4652ff305e16d5ca9f8efc25ad3cbc16d2acd7c8
DimaDemchenko <ipz215_ddo@student.ztu.edu.ua>
2023-04-26 23:11:17 +0300

    added TaskProvider

--- ToDoList/Services/TaskProvider.cs
+++ ToDoList/Services/TaskProvider.cs
@@ -1,6 +1,32 @@
-﻿namespace ToDoList.Services
+﻿using System.Data;
+using ToDoList.EnumData;
+using ToDoList.Repository;
+
+namespace ToDoList.Services
 {
     public class TaskProvider
     {
+        private readonly CookieService _cookieService;
+        private readonly IDbConnection _connection;
+
+        public TaskProvider(CookieService cookieService, IDbConnection connection)
+        {
+            _cookieService = cookieService;
+            _connection = connection;
+        }
+
+        public ITasksRepository GetTaskRepository()
+        { 
+            var storageType = _cookieService.Get("Storage");
+
+            if (storageType == StorageType.XML)
+            {
+                return null;
+            }
+            else
+            { 
+                return new TasksRepository(_connection);
+            }
+        }
     }
 }
